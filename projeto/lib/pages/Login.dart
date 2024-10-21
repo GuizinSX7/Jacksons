07e13e047008029466b41c8a115f52214a8adc8f
GuizinSX7 <guilhermeferraresidallacqua405@gmail.com>
2024-10-21 14:55:39 -0300

    login-quase-pronto

--- projeto/lib/pages/Login.dart
+++ projeto/lib/pages/Login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-import 'package:projeto/Shared/style.dart'; // Para usar a constante pi
+import 'package:projeto/Shared/style.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -11,112 +11,153 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  bool _isChecked = false; // Estado da checkbox
+  bool _isChecked = false;
+  bool _obscurePassword = true;
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: MyColors.preto,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 140), // Espaçamento de 140 pixels do topo
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Transform.rotate(
-                  angle: 11.96 * (pi / 180),
-                  child: Image.asset(
-                    'assets/Jacksons.png',
-                    width: 116,
-                    height: 80,
-                  ),
-                ),
-                SizedBox(width: 10),
-                Text(
-                  'Jacksons',
-                  style: TextStyle(
-                    fontSize: 42,
-                    color: MyColors.branco,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 163), // Espaçamento entre a imagem e os inputs
-            // Caixas de texto
-            Column(
-              children: [
-                Container(
-                  width: 300,
-                  height: 44.28, // Altura do input
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Seu e-mail',
-                      hintStyle: TextStyle(
-                        color: MyColors.pretotransparente,
-                        height: 1.0, // Ajusta a altura da linha
-                      ),
-                      filled: true,
-                      fillColor: MyColors.cinzacaixatexto, // Cor de fundo da caixa
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(102.0), // Bordas arredondadas
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: 11.0,
-                        horizontal: 16.0,
-                      ), // Ajuste vertical
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20), // Espaçamento entre os inputs
-                Container(
-                  width: 300,
-                  height: 44.28, // Altura do input
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Sua senha',
-                      hintStyle: TextStyle(
-                        color: MyColors.pretotransparente,
-                        height: 1.0, // Ajusta a altura da linha
-                      ),
-                      filled: true,
-                      fillColor: MyColors.cinzacaixatexto, // Cor de fundo da caixa
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(102.0), // Bordas arredondadas
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: 11.0,
-                        horizontal: 16.0,
-                      ), // Ajuste vertical
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20), // Espaçamento entre os inputs e a checkbox
+                SizedBox(height: 140),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Checkbox(
-                      value: _isChecked, // Estado da checkbox
-                      onChanged: (bool? value) {
-                        setState(() {
-                          _isChecked = value ?? false; // Atualiza o estado
-                        });
-                      },
+                    Transform.rotate(
+                      angle: 11.96 * (pi / 180),
+                      child: Image.asset(
+                        'assets/Jacksons.png',
+                        width: 116,
+                        height: 80,
+                      ),
                     ),
+                    SizedBox(width: 10),
                     Text(
-                      'Lembre-se de mim',
+                      'Jacksons',
                       style: TextStyle(
-                        color: MyColors.branco, // Cor do texto da checkbox
+                        fontSize: 42,
+                        color: MyColors.branco,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 163),
+                Column(
+                  children: [
+                    _buildTextField(
+                      controller: _emailController,
+                      hint: 'Seu e-mail',
+                      obscureText: false,
+                    ),
+                    SizedBox(height: 20),
+                    _buildTextField(
+                      controller: _passwordController,
+                      hint: 'Sua senha',
+                      obscureText: _obscurePassword,
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscurePassword ? Icons.visibility_off : Icons.visibility,
+                          color: MyColors.pretotransparente, // Ajuste temporário
+                        ),
+                        onPressed: () {
+                          print('Ícone pressionado'); // Debug
+                          setState(() {
+                            _obscurePassword = !_obscurePassword;
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Checkbox(
+                          value: _isChecked,
+                          activeColor: MyColors.roxo,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              _isChecked = value ?? false;
+                            });
+                          },
+                        ),
+                        Text(
+                          'Lembre-se de mim',
+                          style: TextStyle(
+                            color: MyColors.branco,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 40),
+                    Container(
+                      width: 285,
+                      height: 50.77,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle login logic here
+                        },
+                        child: Text(
+                          'Logar',
+                          style: TextStyle(fontSize: 24, color: MyColors.branco),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: MyColors.roxo,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
                       ),
                     ),
                   ],
                 ),
               ],
             ),
-          ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String hint,
+    bool obscureText = false,
+    Widget? suffixIcon,
+  }) {
+    return Container(
+      width: 330,
+      height: 44.28,
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          hintText: hint,
+          hintStyle: TextStyle(
+            color: MyColors.pretotransparente,
+            height: 1.0,
+            fontSize: 18,
+          ),
+          filled: true,
+          fillColor: MyColors.cinzacaixatexto,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(102.0),
+            borderSide: BorderSide.none,
+          ),
+          contentPadding: EdgeInsets.symmetric(
+            vertical: 11.0,
+            horizontal: 16.0,
+          ),
+          suffixIcon: suffixIcon,
         ),
       ),
     );
