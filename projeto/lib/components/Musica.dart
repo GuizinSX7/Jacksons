aa77faefcf4bd959f9227474e532e32a0e56fa16
juan12331 <juanxadez2@gmail.com>
2024-11-01 09:38:36 -0300

    Arrumando provisóriamente musica.dart (widget de musica) para que não fique horrivel

--- projeto/lib/components/Musica.dart
+++ projeto/lib/components/Musica.dart
@@ -10,6 +10,6 @@ class Musica extends StatefulWidget {
 class _MusicaState extends State<Musica> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
